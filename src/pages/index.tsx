a3e98b25fa14d56c61b1c85499d95dd5fde62510
shred-der <teminimighty@gmail.com>
2023-03-27 21:48:57 +0100

    first changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import type { NextPage } from "next";
 import SignIn from "../components/Signin";
-import Header from '@/components/Header';
-import Footer from "../components/Footer";
-import Subsection from "../components/Subsection";
 
 const HomePage: NextPage = () => {
 
