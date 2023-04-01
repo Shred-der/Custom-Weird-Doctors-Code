66b0656129e54b9110933624605e686c3e3827d4
shred-der <teminimighty@gmail.com>
2023-04-01 23:31:29 +0100

    front-end added

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -7,6 +7,12 @@ import { CardanoWallet } from '@meshsdk/react';
 import { useWallet, useAssets } from '@meshsdk/react';
 import { BlockfrostProvider } from '@meshsdk/core';
 import MetaDataCheck from './metadatacheck';
+import styles from "../styles/Signin.module.css";
+import Image from 'next/image';
+import logo1 from "../images/weirddoctors.png";
+import Loading from './Loading';
+
+
 
 export default function SignIn() {
   const { connected, wallet } = useWallet();
@@ -108,8 +114,8 @@ export default function SignIn() {
   // 1. (i) The user is signed into Discord and connected to wallet and has Gold CNFT
   if (connected && session && hasGold && !hasSilver && !hasPlatinum && !hasAir) {
     return (
-      <div>
-        <div>
+      <div className={styles.container}>
+        <div className={styles.btncontent}>
         <h1>You have the required NFT with the Gold Medic property</h1>
         <button onClick={() => grantGoldRole()} disabled={disabled}>
             {loading ? "Please Wait..." : "Give me the Gold role!"}
@@ -128,8 +134,8 @@ export default function SignIn() {
   // 1. (ii) The user is signed into Discord and connected to wallet and has Silver CNFT
   if (connected && session && hasSilver && !hasGold && !hasPlatinum && !hasAir) {
     return (
-      <div>
-        <div>
+      <div className={styles.container}>
+        <div className={styles.btncontent}>
           <h1>You have the required NFT with the Silver Medic property</h1>
         <button onClick={() => grantSilverRole()} disabled={disabled}>
             {loading ? "Please Wait..." : "Give me the Silver role!"}
@@ -148,8 +154,8 @@ export default function SignIn() {
   // 1. (iii) The user is signed into Discord and connected to wallet and has Platinum CNFT
   if (connected && session && hasPlatinum && !hasGold && !hasSilver && !hasAir) {
     return (
-      <div>
-        <div>
+      <div className={styles.container}>
+        <div className={styles.btncontent}>
         <h1>You have the required NFT with the Platinum Medic property</h1>
         <button onClick={() => grantPlatinumRole()} disabled={disabled}>
             {loading ? "Please Wait..." : "Give me the Platinum role!"}
@@ -168,8 +174,8 @@ export default function SignIn() {
    // 1. (iv) The user is signed into Discord and connected to wallet and has Air CNFT
    if ( connected && session && hasAir && !hasGold && !hasSilver && !hasPlatinum) {
     return (
-      <div>
-        <div>
+      <div className={styles.container}>
+        <div className={styles.btncontent}>
         <h1>You have the required NFT with the Air Medic property</h1>
         <button onClick={() => grantAirRole()} disabled={disabled}>
             {loading ? "Please Wait..." : "Give me the Air role!"}
@@ -186,8 +192,8 @@ export default function SignIn() {
      // 1. (v) The user is signed into Discord and connected to wallet and has Silver, Gold, Platinum and Air
      if ( connected && session && hasSilver && hasGold && hasPlatinum && hasAir) {
       return (
-        <div>
-          <div>
+        <div className={styles.container}>
+        <div className={styles.btncontent}>
           <h1>You have the Silver, Gold, Platinum and Air NFTs</h1>
           <button onClick={() => grantSilverRole()} disabled={disabled}>
               {loading ? "Please Wait..." : "Give me the Silver role!"}
@@ -214,8 +220,8 @@ export default function SignIn() {
     // 1. (vi) The user is signed into Discord and connected to wallet and has Silver and Gold
     if ( connected && session && hasSilver && hasGold && !hasPlatinum && !hasAir)  {
       return (
-        <div>
-          <div>
+        <div className={styles.container}>
+        <div className={styles.btncontent}>
           <h1>You have the Silver and Gold NFTs</h1>
           <button onClick={() => grantSilverRole()} disabled={disabled}>
               {loading ? "Please Wait..." : "Give me the Silver role!"}
@@ -235,8 +241,8 @@ export default function SignIn() {
     // 1. (vii) The user is signed into Discord and connected to wallet and has Silver and Platinum
     if ( connected && session && hasSilver && hasPlatinum && !hasGold && !hasAir) {
       return (
-        <div>
-          <div>
+        <div className={styles.container}>
+        <div className={styles.btncontent}>
           <h1>You have the Silver and Platinum NFTs</h1>
           <button onClick={() => grantSilverRole()} disabled={disabled}>
               {loading ? "Please Wait..." : "Give me the Silver role!"}
@@ -257,8 +263,8 @@ export default function SignIn() {
     // 1. (viii) The user is signed into Discord and connected to wallet and has Silver and Air
     if ( connected && session && hasSilver && hasAir && !hasGold && !hasPlatinum) {
       return (
-        <div>
-          <div>
+        <div className={styles.container}>
+        <div className={styles.btncontent}>
           <h1>You have the Silver and Air NFTs</h1>
           <button onClick={() => grantSilverRole()} disabled={disabled}>
               {loading ? "Please Wait..." : "Give me the Silver role!"}
@@ -279,8 +285,8 @@ export default function SignIn() {
     // 1. (ix) The user is signed into Discord and connected to wallet and has Silver, Gold, Platinum and Air
     if ( connected && session && hasGold && hasPlatinum && !hasSilver && !hasAir) {
       return (
-        <div>
-          <div>
+        <div className={styles.container}>
+        <div className={styles.btncontent}>
           <h1>You have the  Gold and Platinum NFTs</h1>
             <button onClick={() => grantGoldRole()} disabled={disabled}>
               {loading ? "Please Wait..." : "Give me the Gold role!"}
@@ -301,8 +307,8 @@ export default function SignIn() {
     // 1. (x) The user is signed into Discord and connected to wallet and has Gold and Air
     if ( connected && session && hasGold && hasAir && !hasSilver && !hasPlatinum) {
       return (
-        <div>
-          <div>
+        <div className={styles.container}>
+        <div className={styles.btncontent}>
           <h1>You have the Gold and Air NFTs</h1>
             <button onClick={() => grantGoldRole()} disabled={disabled}>
               {loading ? "Please Wait..." : "Give me the Gold role!"}
@@ -323,8 +329,8 @@ export default function SignIn() {
     // 1. (xi) The user is signed into Discord and connected to wallet and has Platinum and Air
     if ( connected && session && hasPlatinum && hasAir && !hasSilver && !hasGold) {
       return (
-        <div>
-          <div>
+        <div className={styles.container}>
+        <div className={styles.btncontent}>
           <h1>You have the Platinum and Air NFTs</h1>
             <button onClick={() => grantPlatinumRole()} disabled={disabled}>
               {loading ? "Please Wait..." : "Give me the Platinum role!"}
@@ -345,8 +351,8 @@ export default function SignIn() {
     // 1. (xii) The user is signed into Discord and connected to wallet and has Silver, Gold and Air
     if ( connected && session && hasSilver && hasGold && hasAir && !hasPlatinum) {
       return (
-        <div>
-          <div>
+        <div className={styles.container}>
+        <div className={styles.btncontent}>
           <h1>You have the Silver, Gold and Air NFTs</h1>
           <button onClick={() => grantSilverRole()} disabled={disabled}>
               {loading ? "Please Wait..." : "Give me the Silver role!"}
@@ -370,8 +376,8 @@ export default function SignIn() {
     // 1. (xiii) The user is signed into Discord and connected to wallet and has Silver, Gold and Platinum
     if ( connected && session && hasSilver && hasGold && hasPlatinum && !hasAir) {
       return (
-        <div>
-          <div>
+        <div className={styles.container}>
+        <div className={styles.btncontent}>
           <h1>You have the Silver, Gold and Platinum NFTs</h1>
           <button onClick={() => grantSilverRole()} disabled={disabled}>
               {loading ? "Please Wait..." : "Give me the Silver role!"}
@@ -394,36 +400,40 @@ export default function SignIn() {
 
 
 
-  // 2. Connect Wallet
-  if (!connected ) {
-    return (
-      <div>
-        
-        <div> 
-          <p> Get Access to our restricted contents by holding our NFT(s)</p>
+    // 2. Connect Wallet
+    if (!connected) {
+      return (
+        <div className={styles.container}>
+          
+          <div className={styles.title}> 
+            <h1>Weird Doctors</h1>
+          </div>
+          <div className={styles.imgcontainer}>
+          <Image src={logo1} alt={"Credit-Crusader"} className={styles.img} />
+          </div>
+          <div className={styles.content}>
+            <Link href={"https://www.jpg.store/collection/weirddoctorsvip?tab=items"}>
+            <button>
+              BUY NFT PASS
+            </button>
+            </Link>
+          </div>
+          <div className={styles.content}>
+            <h1> You have our CNFT? Get Discord Role!</h1>
+            <h2 >Discord Membership Access</h2>
+            <CardanoWallet />
+          </div>
+  
         </div>
-        <div>
-        </div>
-        <div>
-          <button>
-            GET MEMBERSHIP CNFT
-          </button>
-        </div>
-        <div>
-          <h1> You have our CNFT? Join Discord!</h1>
-          <h2 >Discord Membership Access</h2>
-          <CardanoWallet />
-        </div>
+      );
+    }
 
-      </div>
-    );
-  }
 
   // 3. Connect with Discord (OAuth)
   if (!session) {
     return (
-      <div>
-        <div>
+      <div className={styles.container}>
+      <div className={styles.btncontent}>
           <h2 >Sign In with Discord</h2>
           <p>Sign In with Discord to check your eligibility for the NFT!</p>
 
@@ -442,18 +452,19 @@ export default function SignIn() {
   // 4. If connected and session and !haspolicyIdasset
   if (loading) {
     return (
-     <div><h1>Loading...</h1></div>
+      <Loading />
     );
   }
 
   if  (connected && session && !hasGold && !hasSilver && !hasPlatinum && !hasAir) {
     return (
-      <div>
-        <div>
-          <p> You don't have the required NFT</p>
+      <div className={styles.container}>
+      <div className={styles.btncontent}>
+          <Link href={"https://www.jpg.store/collection/weirddoctorsvip?tab=items"}></Link>
           <button>
             GET MEMBERSHIP CNFT
           </button>
+          <p> You don't have the required NFT</p>
         </div>
       </div>
 
